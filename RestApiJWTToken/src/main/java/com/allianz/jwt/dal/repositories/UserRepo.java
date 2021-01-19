5d44d7bfa57711fd1678ef62022a6a094d7190f4
abdelrahmanaali <abdelrahman.aali@hotmail.com>
2021-01-19 17:54:16 +0200

    V1

--- RestApiJWTToken/src/main/java/com/allianz/jwt/dal/repositories/UserRepo.java
+++ RestApiJWTToken/src/main/java/com/allianz/jwt/dal/repositories/UserRepo.java
@@ -7,4 +7,5 @@ import com.allianz.jwt.dal.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
 	List<User> findByUsername(String username);
+	List<User> findByUsernameAndPassword(String username, String password);
 }
