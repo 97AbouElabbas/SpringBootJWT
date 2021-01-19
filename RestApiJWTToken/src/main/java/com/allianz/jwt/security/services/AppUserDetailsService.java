d068a21ca451d33c792276b18c1df6da988ff098
abdelrahmanaali <abdelrahman.aali@hotmail.com>
2021-01-19 10:56:13 +0200

    V1

--- RestApiJWTToken/src/main/java/com/allianz/jwt/security/services/AppUserDetailsService.java
+++ RestApiJWTToken/src/main/java/com/allianz/jwt/security/services/AppUserDetailsService.java
@@ -2,18 +2,23 @@ package com.allianz.jwt.security.services;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import com.allianz.jwt.dal.repositories.UserRepo;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService{
-
+	@Autowired
+	UserRepo userrepo;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return new User("admin", "admin", new ArrayList<>());
+		com.allianz.jwt.dal.entities.User user = userrepo.findByUsername(username).get(0);
+		return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
 	}
 
 }
